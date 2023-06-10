bed7ed1181419a844d746a753d4b72f581bc5883
Shakibur Rahman <107750386+SHAKIBUR-404@users.noreply.github.com>
2023-06-10 23:18:02 +0600

    Update Shakib.py

--- Shakib.py
+++ Shakib.py
@@ -1,9 +1,9 @@
 import os, platform, sys, time
 
 os.system('git pull')
-import requests
-bit = platform.architecture()[0]
-if bit == '64bit':
+
+
+
     print("\n\x1b[1;92m Congratulations ! Your Device Support this Tools \n  \n \033[1;31mFOLLOW MY GITHUB")
     os.system('xdg-open https://github.com/SHAKIBUR-404');time.sleep(2)   
 import RND
