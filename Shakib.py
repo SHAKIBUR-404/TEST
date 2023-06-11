d9daffe384e18416b0e96005c7f15fef4b14f5d4
Shakibur Rahman <107750386+SHAKIBUR-404@users.noreply.github.com>
2023-06-11 09:59:57 +0600

    Add files via upload

---
+++ Shakib.py
@@ -0,0 +1,16 @@
+import os, platform, time
+try:
+    import requests
+except:
+    os.system('pip install requests')
+os.system('git pull')
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    print("\n\x1b[1;92m Congratulations ! Your Device Support this Tools \n  \n \033[1;31mFOLLOW MY GITHUB")
+    os.system('xdg-open https://github.com/SHAKIBUR-404');time.sleep(2)   
+    import RND
+   
+elif bit == '32bit':
+    print("\n\x1b[1;92m Congratulations ! Your Device Support this Tools \n Your Device 32 Bit\n  \n \033[1;31mFOLLOW MY GITHUB")
+    import RND2
